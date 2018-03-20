4555f954805be45b684b24bfa4bda596ec9696bd
GabbyMont <gemo1236@gmail.com>
2018-03-20 13:17:58 -0400

    session change

--- app.rb
+++ app.rb
@@ -1,78 +1,97 @@
 require "sinatra"
+enable :sessions
+=begin
+	get "/" do
+		shows
+	end
+	post "/" do
+		creates
+	end
+	...
+	put: replaces
+	patch: modifies
+	delete: deletes/'annihilates'
+	options: 'appeases'
+	link: affiliates
+	unlink: seperates
+=end
 
 get "/" do # directs to the home page (root directory). 'get' goes to a page
-	erb :first_page # directs from 'first_page' erb(embedded ruby) file
+	erb :first_page # directs from 'first_page' erb(embedded ruby) file. (Shows something)
 end 
+
 post "/name" do # 'post' comes from a page. 'name' is the action that comes from the erb file
-	name = params[:name] # brings parameters from input forms, from the erb
-	redirect "/name_redirect?name="+ name 
+	 # brings parameters from input forms, from the erb
+	session[:name] = params[:name]
+	redirect "/name_redirect"
 	#'?' seperates where I am redirecting to and the values being passed through
 end # redirecting you from the end of the erb after submitting to a new get. The variable '+name' is returning the value of what name is. 
 
+=begin
+ERB(embedded ruby file); HTML code with additional syntax
+The <%...%> is a tag that executes a ruby code within them wihtout returning a value to the page
+The <%=...> is a tag that executes a ruby code within it, and prints the returned value of the ruby code.(works like string interpolation in a string that is printed)
+In order to use erb, you will need 'erb :' with a space in between the word erb and the colon. The colon is required to be directly in front of the file name you need.
+	For example, the 'erb :filename' has Sinatra look in the 'views' folder and find the file that is asked for.
+Notes/Comments in erb files
+	<!-- your comment here -->
+
+=end
+
 get "/name_redirect" do
-	f_name = params[:name]
-	erb :second_page,locals:{first_name:f_name}
+	#'parameters/params' are specified by putting a colon directly in front of it. If the ':name' symbol which will then be automatically be added to the 'params' hash by Sinatra
+	name = session[:name]
+	erb :second_page,locals:{name:name}
 end
+
 post "/age" do
-	f_name = params[:f_name]
-	age = params[:age]
-	redirect "/favorite_food?f_name="+ f_name + "&age=" + age
+	session[:age] = params[:age]
+	redirect "/favorite_food"
 end
 
 get "/favorite_food" do
-	f_name = params[:f_name]
-	age = params[:age]
-	erb :third_page, locals:{f_name:f_name, age:age}
+	name = session[:name]
+	erb :third_page, locals:{name:name}
 end
+
 post "/food_drink" do
-	# "hello world"
-	
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	redirect "/favorite_numbers?f_name="+ f_name + "&age=" + age + "&food=" + food + "&drink=" + drink
+	session[:food] = params[:food]
+	session[:drink] = params[:drink]
+	redirect "/favorite_numbers"
 end
 
 get "/favorite_numbers" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	erb :fourth_page, locals:{f_name:f_name, age:age, food:food, drink:drink}
+	name = session[:name]
+	erb :fourth_page, locals:{name:name}
 end
+
 post "/numbers" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	num1 = params[:number_1]
-	num2 = params[:number_2]
-	num3 = params[:number_3]
-	redirect "/favorite_colors?f_name="+ f_name + "&age=" + age + "&food=" + food + "&drink=" + drink + "&number_1=" + num1 + "&number_2=" + num2 + "&number_3=" + num3
+	session[:num1] = params[:num1]
+	session[:num2] = params[:num2]
+	session[:num3] = params[:num3]
+	redirect "/favorite_colors"
 end
 
 get "/favorite_colors" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	num1 = params[:number_1]
-	num2 = params[:number_2]
-	num3 = params[:number_3]
-	erb :fifth_page, locals:{f_name:f_name, age:age, food:food, drink:drink, number_1:num1, number_2:num2, number_3:num3}
+	name = session[:name]
+	session[:colors] = params[:colors]
+	erb :fifth_page, locals:{name:name}
 end
+
 post "/colors" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	num1 = params[:number_1]
-	num2 = params[:number_2]
-	num3 = params[:number_3]
-	fav_colors = params[:colors]
-	erb :sixth_page, locals:{f_name:f_name, age:age, food:food, drink:drink, number_1:num1, number_2:num2, number_3:num3, colors:fav_colors}
+	session[:colors] = params[:colors]
+	redirect "/results"
+	#erb :sixth_page, locals:{name:name, age:age, food:food, drink:drink, number_1:num1, number_2:num2, number_3:num3, colors:fav_colors}
 end
 
-	
-
+get "/results" do
+	name = session[:name]
+	age = session[:age]
+	food = session[:food]
+	drink = session[:drink]
+	num1 = session[:num1]
+	num2 = session[:num2]
+	num3 = session[:num3]
+	colors = session[:colors]
+	erb :sixth_page, locals:{name:name, age:age, food:food, drink:drink, num1:num1, num2:num2, num3:num3, colors:colors}
+end
